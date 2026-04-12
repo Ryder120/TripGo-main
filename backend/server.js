6872bd87fe8b90c9b58c91ab0e5c603819911107
Ryder120 <buzzrocky35@gmail.com>
2026-04-12 20:34:13 +0530

    fix server

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,11 @@ import userRouter from "./routes/userRoutes.js";
 import bookingRouter from "./routes/bookingRoute.js";
 import tourRouter from "./routes/tourRoute.js";
 
-const PORT = process.env.PORT || 4000;
 const app = express();
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
-await connectDB();
 
 app.use("/api/user", userRouter);
 app.use("/api/bookings", bookingRouter);
@@ -21,6 +20,18 @@ app.get("/", (req, res) => {
   res.send("API is Working!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// ✅ FIX HERE
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1); // important
+  }
+};
+
+startServer();
