ad42d00c514cdc0dfaa032910733c6df99be040c
Ryder120 <buzzrocky35@gmail.com>
2026-04-13 11:46:30 +0530

    fix mongodb

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => {
-    console.log("Connected to MongoDB");
-  });
-  await mongoose.connect(`${process.env.MONGODB_URI}/TripGo`);
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log("MongoDB Connected");
 };
 
 export default connectDB;
