25df1ef45b8ed2794c29e8fbf55c4c65999c9d9d
Ryder120 <buzzrocky35@gmail.com>
2026-04-13 12:07:39 +0530

    fix mongodb env

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URI);
-  console.log("MongoDB Connected");
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB Error:", error);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
